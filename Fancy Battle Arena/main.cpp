d90f730866c7f0ede7c7f34188c778e9931c0d05
Tahsin Samudra <tahsinsamudra.110@gmail.com>
2026-08-29 20:16:09 +0600

    player moves ! Next : add clash process

--- Fancy Battle Arena/main.cpp
+++ Fancy Battle Arena/main.cpp
@@ -2,136 +2,131 @@
 #include <cmath>
 
 float angle = 0;
+float tx = 0.0, ty = 0.0;
 
-void display() {
-    glClearColor(0.40f, 0.80f, 0.40f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void Player()
+{
+    glTranslatef(tx, ty, 0);
 
-    //player
-    //Head
-    glColor3f(1.00,0.87,0.80);
+    glColor3f(1.00, 0.87, 0.80);
+
     float cX = 0.0;
     float cY = 1.7;
     float r = 0.7;
 
     glBegin(GL_POLYGON);
 
-    for(int i=0; i<=360; i++)
+    for(int i = 0; i <= 360; i++)
     {
-        float angle = i*3.1416/180;
+        float angle = i * 3.1416 / 180;
 
-        float x =cX+r*cos(angle);
-        float y =cY+r*sin(angle);
+        float x = cX + r * cos(angle);
+        float y = cY + r * sin(angle);
 
         glVertex2f(x, y);
     }
 
     glEnd();
 
+    glColor3f(1.00, 0.87, 0.80);
 
-    //chest
-    glColor3f(1.00,0.87,0.80);
     glBegin(GL_TRIANGLES);
 
         glVertex2f(-0.4, 1);
         glVertex2f(0.4, 1);
-        glVertex2f(0,0.32);
-
+        glVertex2f(0, 0.32);
 
     glEnd();
 
-    //Belt
-    glColor3f(0,0,1);
+    glColor3f(0, 0, 1);
+
     glBegin(GL_QUADS);
 
-        glVertex2f(-0.7,-0.1);
-        glVertex2f(0.7,-0.1);
-        glVertex2f(0.7,-0.4);
-        glVertex2f(-0.7,-0.4);
+        glVertex2f(-0.7, -0.1);
+        glVertex2f(0.7, -0.1);
+        glVertex2f(0.7, -0.4);
+        glVertex2f(-0.7, -0.4);
 
     glEnd();
 
-
-    //body
     glColor3f(1.0f, 0.65f, 0.25f);
+
     glBegin(GL_QUADS);
 
-        glVertex2f(-.7,-1);
-        glVertex2f(-.7,1);
-        glVertex2f(.7,1);
-        glVertex2f(.7,-1);
+        glVertex2f(-0.7, -1);
+        glVertex2f(-0.7, 1);
+        glVertex2f(0.7, 1);
+        glVertex2f(0.7, -1);
 
     glEnd();
 
+    glColor3f(0, 0, 1);
 
-
-    //left leg
-    glColor3f(0,0,1);
     glBegin(GL_QUADS);
 
-        glVertex2f(-.7,-1);
-        glVertex2f(0,1);
-        glVertex2f(0,-1.8);
-        glVertex2f(-.7,-1.8);
-
-    glEnd();
-    //right leg
-    glColor3f(0,0,1);
-    glBegin(GL_QUADS);
-
-        glVertex2f(0.1,-1);
-        glVertex2f(0.7,-1);
-        glVertex2f(0.7,-1.8);
-        glVertex2f(0.1,-1.8);
+        glVertex2f(-0.7, -1);
+        glVertex2f(0, 1);
+        glVertex2f(0, -1.8);
+        glVertex2f(-0.7, -1.8);
 
     glEnd();
 
+    glColor3f(0, 0, 1);
 
+    glBegin(GL_QUADS);
 
-    //health Cube
-    glPushMatrix();
+        glVertex2f(0.1, -1);
+        glVertex2f(0.7, -1);
+        glVertex2f(0.7, -1.8);
+        glVertex2f(0.1, -1.8);
+
+    glEnd();
+
+    glTranslatef(-tx, -ty, 0);
+}
+
+void display()
+{
+    glClearColor(0.40f, 0.80f, 0.40f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    Player();
 
     glTranslatef(25, -25, 0);
     glRotatef(angle, 1, 1, 0);
 
     glBegin(GL_QUADS);
 
-        // Front
         glColor3f(1, 0, 0);
         glVertex3f(-2, -2, 2);
         glVertex3f(2, -2, 2);
         glVertex3f(2, 2, 2);
         glVertex3f(-2, 2, 2);
 
-        // Back
         glColor3f(0, 1, 0);
         glVertex3f(-2, -2, -2);
         glVertex3f(-2, 2, -2);
         glVertex3f(2, 2, -2);
         glVertex3f(2, -2, -2);
 
-        // Left
         glColor3f(0, 0, 1);
         glVertex3f(-2, -2, -2);
         glVertex3f(-2, -2, 2);
         glVertex3f(-2, 2, 2);
         glVertex3f(-2, 2, -2);
 
-        // Right
         glColor3f(1, 1, 0);
         glVertex3f(2, -2, -2);
         glVertex3f(2, 2, -2);
         glVertex3f(2, 2, 2);
         glVertex3f(2, -2, 2);
 
-        // Top
         glColor3f(0, 1, 1);
         glVertex3f(-2, 2, -2);
         glVertex3f(-2, 2, 2);
         glVertex3f(2, 2, 2);
         glVertex3f(2, 2, -2);
 
-        // Bottom
         glColor3f(1, 0, 1);
         glVertex3f(-2, -2, -2);
         glVertex3f(2, -2, -2);
@@ -140,43 +135,63 @@ void display() {
 
     glEnd();
 
-    glPopMatrix();
-
+    glLoadIdentity();
 
     glutSwapBuffers();
 }
 
-void update(int value) {
-
+void update(int value)
+{
     angle += 1;
 
     glutPostRedisplay();
-    glutTimerFunc(20, update, 0);
+    glutTimerFunc(3, update, 0);
 }
 
+void keyboard(unsigned char c, int x, int y)
+{
+    if(c == 'w')
+    {
+        ty += 1;
+    }
+    else if(c == 's')
+    {
+        ty -= 1;
+    }
+    else if(c == 'a')
+    {
+        tx -= 1;
+    }
+    else if(c == 'd')
+    {
+        tx += 1;
+    }
 
+    glutPostRedisplay();
+}
 
-
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
     glutInit(&argc, argv);
 
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
     glutInitWindowSize(1980, 1080);
     glutCreateWindow("3D Objects");
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
     glOrtho(-30, 30, -30, 30, -30, 30);
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
     glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
-
     glutTimerFunc(16, update, 0);
+    glutKeyboardFunc(keyboard);
 
     glutMainLoop();
 
